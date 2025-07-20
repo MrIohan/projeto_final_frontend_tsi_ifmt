0871f6ffbef0669ad918459b2bb5b1809aeb17e9
MrIohan <joao.mello.dev369@gmail.com>
2025-07-19 23:58:18 -0400

    faze final

---
+++ Modulos/Atividade_2/tcc.php
@@ -0,0 +1,28 @@
+<div class="tcc-box">
+    <div class="box-01 box">
+        <h1>Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso</h1>
+        <h2>Campus Cuiabá Cel. Octayde Jorge da Silva</h2>
+    </div>
+
+    <div class="box-02 box">
+        <h3>Tecnologia em Sistemas para Internet</h3>
+        <h3>Programação Web Front-End</h3>
+    </div>
+
+    <div class="box-03 box">
+        <h1>Prova Final</h1>
+    </div>
+
+    <div class="box-04 box">
+        <h3>1º Semestre 2025/1</h3>
+    </div>
+
+    <div class="box-05 box">
+        <h2>João Vítor de Mello Oliveira Smiderle</h2>
+    </div>
+
+    <div class="box-06 box">
+        <h3>19/07/2025</h3>
+        <h3>Prof. Felipe</h3>
+    </div>
+</div>
