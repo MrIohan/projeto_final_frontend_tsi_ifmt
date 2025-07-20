7edb79e733780faae2f7dfe2718d28285d83617b
MrIohan <joao.mello.dev369@gmail.com>
2025-07-20 00:02:25 -0400

    só a logo na guia

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Assets/css/index.css">
     <link rel="stylesheet" href="../../Assets/css/@CONTEUDO@.css">
+    <link rel="icon" type="image/png" href="/Assets/img/icon.png">
     <title>@TITULO@</title>
 </head>
 
